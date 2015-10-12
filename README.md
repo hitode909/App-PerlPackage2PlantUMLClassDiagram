f439d11ccad3643bb4dac8e8dcded541cc589c6c
hitode909 <hitode909@gmail.com>
2015-10-12 14:23:24 +0900

    minil new

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::PerlPackage2PlantUMLClassDiagram - It's new $module
+
+# SYNOPSIS
+
+    use App::PerlPackage2PlantUMLClassDiagram;
+
+# DESCRIPTION
+
+App::PerlPackage2PlantUMLClassDiagram is ...
+
+# LICENSE
+
+Copyright (C) hitode909.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+hitode909 <hitode909@gmail.com>
---
+++ lib/App/PerlPackage2PlantUMLClassDiagram.pm
@@ -0,0 +1,39 @@
+package App::PerlPackage2PlantUMLClassDiagram;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::PerlPackage2PlantUMLClassDiagram - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::PerlPackage2PlantUMLClassDiagram;
+
+=head1 DESCRIPTION
+
+App::PerlPackage2PlantUMLClassDiagram is ...
+
+=head1 LICENSE
+
+Copyright (C) hitode909.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+hitode909 E<lt>hitode909@gmail.comE<gt>
+
+=cut
+
